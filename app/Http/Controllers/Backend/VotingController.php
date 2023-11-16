8d7ddd7fe06efc89ea01dba09e76f0a7c2c87f9d
pramana yuda <pramanayuda772@gmail.com>
2023-11-16 23:05:41 +0700

    balikin

--- app/Http/Controllers/Backend/VotingController.php
+++ app/Http/Controllers/Backend/VotingController.php
@@ -81,7 +81,7 @@ class VotingController extends Controller
 
 
 
-        return $list;
+        // return $list;
         $list->transform(function ($item, $key) {
             
             $result = [
